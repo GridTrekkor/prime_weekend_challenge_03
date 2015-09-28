1d63f4489c24949578345341319f0e30d4ef86e9
Disco <jwo@umn.edu>
2015-09-28 09:34:20 -0500

    Circle indicators

--- client/app.js
+++ client/app.js
@@ -8,10 +8,22 @@ $(document).ready(function() {
         url: "/data.json"
     }).done(function(data) {
 
+        // append circle indicators
+        for (var i = 0; i < data.length; i++) {
+            $(".carousel-indicators").append("<li data-target='#myCarousel' data-slide-to='" +  i + "' title='" + data[i].Name + "'></li>");
+        }
+
+        // click on a circle indicator
+        $(".carousel-indicators li").click(function() {
+            currentPerson = $(this).data("slide-to");
+            displayInfo(currentPerson);
+        });
+
         displayInfo(currentPerson);
 
         var max = (data.length) - 1;
 
+        // click previous or next arrows
         $("#previous").click(function() {
             if (currentPerson == 0) {
                 currentPerson = max;
@@ -30,11 +42,14 @@ $(document).ready(function() {
             displayInfo(currentPerson);
         });
 
+        // output JSON data
         function displayInfo(currentPerson) {
             //console.log(currentPerson);
+            $(".carousel-indicators li").attr("class", "");
             $("#name").text((currentPerson + 1) + ": " + data[currentPerson].Name);
             $("#description").text(data[currentPerson].Description);
             $("#shoutout").text(data[currentPerson].Shoutout);
+            $(".carousel-indicators li").eq(currentPerson).attr("class", "active");
         }
 
     });
